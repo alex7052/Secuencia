4401da3e68079498912f5f0e001744b4d89d81af
alex7052 <50718133+alex7052@users.noreply.github.com>
2019-05-17 20:38:49 -0500

    Initial commit

---
+++ src/test/java/com/cystera/secuencia/SecuenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cystera.secuencia;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SecuenciaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
